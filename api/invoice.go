9fe22db7bb96693a6886bd32183e76f5547d11d3
kiwiidb <kwinten.debacker@hotmail.com>
2021-06-01 14:07:30 +0200

    try different go build

--- api/invoice.go
+++ api/invoice.go
@@ -13,47 +13,45 @@ import (
 	"github.com/ipsn/go-libtor"
 )
 
-func init() {
-
-}
-
-func run() error {
+func startTorAndGetDialer() (dialer *tor.Dialer, err error) {
 	// Start tor with default config (can set start conf's DebugWriter to os.Stdout for debug logs)
 	fmt.Println("Starting tor and fetching title of https://check.torproject.org, please wait a few seconds...")
 	t, err := tor.Start(nil, &tor.StartConf{ProcessCreator: libtor.Creator, DebugWriter: os.Stderr})
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer t.Close()
 	// Wait at most a minute to start network and get
 	dialCtx, dialCancel := context.WithTimeout(context.Background(), time.Minute)
 	defer dialCancel()
 	// Make connection
-	dialer, err := t.Dialer(dialCtx, nil)
+	dialer, err = t.Dialer(dialCtx, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
+	return dialer, nil
+}
+
+func getInfo(dialer *tor.Dialer, onion, macaroon string) (info string, err error) {
+
 	tr := &http.Transport{
 		DialContext:     dialer.DialContext,
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	httpClient := &http.Client{Transport: tr}
-	// Get /
-	fmt.Println("making request")
-	req, err := http.NewRequest("GET", "todo", nil)
+	fmt.Println("making request over tor")
+	req, err := http.NewRequest("GET", onion, nil)
 	if err != nil {
-		return err
+		return "", err
 	}
-	req.Header.Set("Grpc-Metadata-macaroon", "todo")
+	req.Header.Set("Grpc-Metadata-macaroon", macaroon)
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer resp.Body.Close()
-	// Grab the <title>
 	result, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf(string(result))
-	return nil
+	return string(result), nil
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
